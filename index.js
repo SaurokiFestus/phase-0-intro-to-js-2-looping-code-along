e5c9ab01e800b72121be03aff11a130385e5ce12
SaurokiFestus <sauroki.festus@student.moringaschool.com>
2022-06-04 20:09:54 +0300

    commit tree 57a889fa18072653f217558939742228ac441a0e

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age ++){
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+// }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+//     for (let i = 0; i < gifts.length; i++){
+//         console.log(`Wrapped ${gifts[i]} and added a bow `);
+//         debugger;
+//     }
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(stringName, eventName){
+    let thankYou = []
+    for(let i = 0; i < stringName.length; i++){
+        thankYou.push(`Thank you, ${stringName[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return thankYou;
+}
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
